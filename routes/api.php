261d3571d5201a7b1425e27a1db0703b1727c6cd
Yan Kairalla <yanmkairalla@hotmail.com>
2020-04-10 04:08:34 -0300

    updated routes files

--- routes/api.php
+++ routes/api.php
@@ -14,18 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::post('auth/login', 'AuthController@login');
-
-Route::get('user', 'UserController@index');
-Route::get('user/{id}', 'UserController@show');
 
 // routes with authentication
 Route::group(['middleware' => ['apiJwt']], function () {
     Route::post('logout', 'AuthController@logout');
+    Route::get('user', 'UserController@index');
+    Route::get('user/{id}', 'UserController@show');
 
     //book routes
     Route::get('book', 'BookController@index');
